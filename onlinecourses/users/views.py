281716b326d192688910b29b3c4386db1d983fe0
Eugene <nord_belik@mail.ru>
2024-01-09 01:12:37 +0300

    Добавлено приложение USERS для регистрации пользователей

---
+++ onlinecourses/users/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from .forms import LoginUserForm
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('users:login')
