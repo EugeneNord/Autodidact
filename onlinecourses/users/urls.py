4b416732bea26e02f4282e18a823f255743c0f99
Eugene <nord_belik@mail.ru>
2024-01-09 18:56:29 +0300

    добавлена своя форма для изменения пароля

--- onlinecourses/users/urls.py
+++ onlinecourses/users/urls.py
@@ -8,7 +8,7 @@ app_name = "users"
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
-    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/', views.UserPasswordChange.as_view(), name='password_change'),
     path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
