3e8d621f313dfb6f82b7877d85f00821df738610
Eugene <nord_belik@mail.ru>
2024-01-09 18:21:56 +0300

    добавлен профиль пользователя с возможность сменить пароль

--- onlinecourses/users/urls.py
+++ onlinecourses/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 from . import views
 
@@ -8,5 +8,8 @@ app_name = "users"
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('register/', views.RegisterUser.as_view(), name='register'),
-]
+    path('profile/', views.ProfileUser.as_view(), name='profile'),
+    ]
